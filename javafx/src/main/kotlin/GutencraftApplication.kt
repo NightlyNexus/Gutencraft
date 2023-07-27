1871ed0447771fd60d022d9edb15fba2ef5b2310
Eric Cochran <Eric@NightlyNexus.com>
2023-07-27 00:45:08 -0400

    Add Bedrock support to JavaFX app.

--- javafx/src/main/kotlin/GutencraftApplication.kt
+++ javafx/src/main/kotlin/GutencraftApplication.kt
@@ -1,5 +1,6 @@
 import javafx.application.Application
 import javafx.geometry.Insets
+import javafx.geometry.Pos.CENTER_LEFT
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.control.Label
@@ -11,6 +12,7 @@ import javafx.scene.input.ClipboardContent
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.CornerRadii
+import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import javafx.stage.Stage
@@ -29,19 +31,30 @@ class GutencraftApplication : Application() {
     val layout = VBox(10.0)
     scrollPane.content = layout
     val input = TextArea()
-    val print = Button("Print")
-    print.setOnAction {
+    val printBar = HBox(10.0).apply { alignment = CENTER_LEFT }
+    val printJava = Button("Print for Java Edition")
+    val orLabel = Label("or")
+    val printBedrock = Button("Print for Bedrock Edition")
+    printBar.children.add(printJava)
+    printBar.children.add(orLabel)
+    printBar.children.add(printBedrock)
+
+    fun printResult(javaEdition: Boolean) {
       layout.children.remove(2, layout.children.size)
       val inputText = input.text
       val pages: List<String>
       try {
-        pages = pagesJava(inputText)
+        pages = if (javaEdition) {
+          pagesJava(inputText)
+        } else {
+          pagesBedrock(inputText)
+        }
       } catch (e: UnsupportedCharacterException) {
         val errorMessage = errorMessage(inputText)
         val errorLabel = Label(errorMessage)
         errorLabel.textFill = Color.color(1.0, 0.0, 0.0)
         layout.children.add(errorLabel)
-        return@setOnAction
+        return
       }
       val command = command(pages)
       layout.addItem(command, "Copy command")
@@ -50,8 +63,19 @@ class GutencraftApplication : Application() {
         layout.addItem(page, "${i + 1}. Copy")
       }
     }
+
+    printJava.setOnAction {
+      printResult(true)
+      printJava.select()
+      printBedrock.deselect()
+    }
+    printBedrock.setOnAction {
+      printResult(false)
+      printJava.deselect()
+      printBedrock.select()
+    }
     layout.children.add(input)
-    layout.children.add(print)
+    layout.children.add(printBar)
     primaryStage.scene = Scene(scrollPane, 600.0, 300.0)
     primaryStage.show()
   }
@@ -83,5 +107,13 @@ class GutencraftApplication : Application() {
         clipboard.setContent(content)
       }
     })
+  }
+
+  private fun Button.select() {
+    style = "-fx-base: #4EB02D;"
+  }
+
+  private fun Button.deselect() {
+    style = ""
   }
 }
